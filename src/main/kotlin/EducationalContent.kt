0576f479b48832486b5b7db385ddda52ebe726ba
ViniciusJMR <viniciusdejesus605@gmail.com>
2023-10-05 18:08:26 -0300

    edit: changed EducationalContent inner class name

--- src/main/kotlin/EducationalContent.kt
+++ src/main/kotlin/EducationalContent.kt
@@ -3,7 +3,13 @@
 // Each have videos and each video have category(?),
 // Can have test
 data class EducationalContent(
-    var name: String,
+    val name: String,
     val duration: Int,
-    val level: Level
-)
+    val level: Level,
+    val sections: List<Section> = listOf()
+) {
+    data class Section(
+        val name: String,
+        val contents: List<Content>,
+    )
+}
