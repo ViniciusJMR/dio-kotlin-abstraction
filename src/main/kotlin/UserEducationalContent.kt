7881c7b887209d212c2c867008c2e00525223036
ViniciusJMR <viniciusdejesus605@gmail.com>
2023-10-05 18:11:46 -0300

    add: UserEducationalContent has its own UserSection list

--- src/main/kotlin/UserEducationalContent.kt
+++ src/main/kotlin/UserEducationalContent.kt
@@ -2,6 +2,10 @@ data class UserEducationalContent(
     val educationalContent: EducationalContent
 ) {
     private var complete = false
+    private val userSections = educationalContent.sections.map {
+        UserSection(it)
+    }
+
     fun completeEducationalContent() {
         complete = true
     }
